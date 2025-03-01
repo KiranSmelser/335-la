fa7894e2bdd314e8bd90d19ee5b270851d9a0dbd
Kiran Smelser <kiran.smelser@gmail.com>
2025-02-28 21:48:23 -0700

    edited buildpath and used file loader to load in albums data

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,18 +5,25 @@ import model.MusicStore;
 import view.View;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        String albumsTxtPath = "src/main/resources/albums.txt";
-        String albumsDirectoryPath = "src/main/resources/albums";
+        URL albumsTxtUrl = Main.class.getClassLoader().getResource("albums.txt");
+        URL albumsDirectoryUrl = Main.class.getClassLoader().getResource("albums");
 
         try {
-            MusicStore store = new MusicStore(albumsTxtPath, albumsDirectoryPath);
+            Path albumsTxtPath = Paths.get(albumsTxtUrl.toURI());
+            Path albumsDirectoryPath = Paths.get(albumsDirectoryUrl.toURI());
+
+            MusicStore store = new MusicStore(albumsTxtPath.toString(), albumsDirectoryPath.toString());
             LibraryModel library = new LibraryModel();
             View view = new View(library, store);
             view.run();
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             System.err.println("Error loading album files: " + e.getMessage());
         }
     }
